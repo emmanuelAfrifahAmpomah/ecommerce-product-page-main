227a1629b4b4625b37901a2bbd49348a327fb29c
Emmanuel A Ampomah <eaampomah@yahoo.com>
2022-10-11 15:48:45 -0700

    added thumbnail

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,26 @@
-alert ('Welcome');
+// $("document").ready(function() {
+//     $("#cartButton").click(function() {
+//         $(".bodyImage").fadeToggle();
+//         $("a:last").css("color", "pink");
+//     });
+// });
+
+
+var productMain = document.getElementById("productMain");
+var productOther = document.getElementsByClassName("productOther");
+
+productOther[0].onclick = function() {
+    productMain.src = productOther[0].src;
+}
+
+productOther[1].onclick = function() {
+    productMain.src = productOther[1].src;
+}
+
+productOther[2].onclick = function() {
+    productMain.src = productOther[2].src;
+}
+
+productOther[3].onclick = function() {
+    productMain.src = productOther[3].src;
+}
